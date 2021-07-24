2dc80f7f3b2250917a7e7b1045fed9dfc6ebd523
Ryota Sasazawa <sasazawa3330@gmail.com>
2021-07-24 12:01:15 +0900

    Revert "add column to vocabs"
    This reverts commit 4a36fc056b8cfbca2a88a1503c81c6020f34bebb.

--- db/migrate/20210724011233_add_speech_to_vocabs.rb
+++
@@ -1,5 +0,0 @@
-class AddSpeechToVocabs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :vocabs, :speech, :text, limit: 16777215, null: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_24_011233) do
+ActiveRecord::Schema.define(version: 2021_07_18_065403) do
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2021_07_24_011233) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "speech", limit: 16777215
   end
 
 end
