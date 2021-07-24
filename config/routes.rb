cf07c18dbfcd9467a7b1e45c5ba6e7f4bf7a4b61
Ryota Sasazawa <sasazawa3330@gmail.com>
2021-07-24 16:35:20 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :vocabs
-      mount_devise_token_auth_for "User", at: "auth"
-    end
-  end
-  namespace :api do
-    namespace :v1 do
       resources :speeches
       mount_devise_token_auth_for "User", at: "auth"
     end
