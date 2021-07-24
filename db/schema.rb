4a36fc056b8cfbca2a88a1503c81c6020f34bebb
Ryota Sasazawa <sasazawa3330@gmail.com>
2021-07-24 11:11:41 +0900

    add column to vocabs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_065403) do
+ActiveRecord::Schema.define(version: 2021_07_24_011233) do
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_07_18_065403) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "speech", limit: 16777215
   end
 
 end
